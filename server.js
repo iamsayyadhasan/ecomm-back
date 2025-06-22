8040c6493f150a76726b800a0f79a43476d93db7
SAYYAD HASAN SHAHZAD <hasanshahzad82@gmail.com>
2025-06-22 20:38:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(cors({
-  origin: "https://clothys.netlify.app", // 🔒 specific allowed origin
-  credentials: true
+  origin: "*"
+ 
 }));
 app.use(express.json());
 
